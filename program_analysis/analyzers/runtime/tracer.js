0c69aec9229a8c46b4e437c9f8bc13bd8eed462d
goelayu@umich.edu <goelayu@umich.edu>
2021-02-18 12:56:39 -0500

    add code to intercept event registeration during page load
    - Don't need this code since chrome provides command line utilites which do the same job
    - Can be used for debugging/better understanding of the event registeration process

--- program_analysis/analyzers/runtime/tracer.js
+++ program_analysis/analyzers/runtime/tracer.js
@@ -30,6 +30,110 @@
         // call the native send()
         _xmlSend.apply(this, arguments);
     }
+
+    // var _plt_super = {
+    //     "addEventListener": Element.prototype.addEventListener,
+    //     "removeEventListener": Element.prototype.removeEventListener,
+    // };
+    
+    // var plt_handlerFieldName = "plt-handlers";
+
+    // elem_with_events = new Set;
+    
+    // // Update element state when a listener is added.
+    // // For each element, we keep track of all unique functions (determined by the function's toString())
+    // // for each type and useCapture option, which matches what the browser does.
+    // // 'replace' is true if the existing handler(s) should be replaced with this one.
+    // function plt_registerListener(el, type, listener, useCapture, replace=false) {
+    //     elem_with_events.add(el);
+    //     var handlers = el[plt_handlerFieldName];
+    //     if (!handlers) {
+    //         handlers = {};
+    //     }
+    //     var uc = useCapture ? "1" : "0";
+    //     if (!handlers[uc]) {
+    //         handlers[uc] = {};
+    //     }
+    //     if (!handlers[uc][type]) {
+    //         handlers[uc][type] = [];
+    //     }
+    //     for (var ix in handlers[uc][type]) {
+    //         if (handlers[uc][type][ix] == listener) {
+    //             return;
+    //         }
+    //     }
+    //     if (replace) {
+    //         handlers[uc][type] = [listener];
+    //     } else {
+    //         handlers[uc][type].push(listener);
+    //     }
+    //     el[plt_handlerFieldName] = handlers;
+    //     Object.defineProperty(el, plt_handlerFieldName, {enumerable: false});
+    // }
+    
+    // // Update element state when a listener is removed.
+    // // 'clearAll' is true if we should wipe all event handlers for this event type (in which case 'listeners' is never used).
+    // function plt_deregisterListener(el, type, listener, useCapture, clearAll=false) {
+    //     var handlers = el[plt_handlerFieldName];
+    //     if (!handlers) {
+    //         return;
+    //     }
+    //     var uc = useCapture ? "1" : "0";
+    //     if (handlers && handlers[uc] && handlers[uc][type]) {
+    //         if (clearAll) {
+    //             handlers[uc].delete(type);
+    //         } else {
+    //             for (var i = 0; i < handlers[uc][type].length; i++) {
+    //                 if (handlers[uc][type][i] == listener) {
+    //                     handlers[uc][type].splice(i, 1);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     el[plt_handlerFieldName] = handlers;
+    // }
+    
+    // // All possible handler-triggering events. Don't modify.
+    // // Source: https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers and some from
+    // // https://developer.mozilla.org/en-US/docs/Web/Events.
+    // var plt_allEventHandlers = ["onabort", "onblur", "onchange", "onclick", "onclose", "oncontextmenu", "ondblclick", "onerror", "onfocus",
+    //     "oninput", "onkeydown", "onkeypress", "onkeyup", "onload", "onmouseenter", "onmousedown", "onmouseleave", "onmousemove", "onmouseout",
+    //     "onmouseover", "onmouseup", "onreset", "onresize", "onscroll", "onselect", "onsubmit" ];
+    
+    // EventTargetProtos = [Element.prototype, Document.prototype]
+    
+    // // Override addEventListener and removeEventListener to use our register / deregister functions first.
+    // EventTargetProtos.forEach((proto)=>{
+    //     proto["addEventListener"] = function(type, listener, useCapture) {
+    //         plt_registerListener(this, type, listener, useCapture);
+    //         _plt_super["addEventListener"].apply(this, arguments);
+    //     }
+        
+    //     proto["removeEventListener"] = function(type, listener, useCapture) {
+    //         plt_deregisterListener(this, type, listener, useCapture);
+    //         _plt_super["removeEventListener"].apply(this, arguments);
+    //     }
+        
+    // });
+  
+    // // Manually override the event listeners set by assigning to object fields.
+    // // Note that 'evt' in this case is of the form "onclick" instead of the "click" argument of
+    // // addEventListener, so the map entries for these two ways to add event listeners will not overlap.
+    // plt_allEventHandlers.forEach(function(evt) {
+    //     [HTMLElement.prototype, Document.prototype].forEach(function(prt) {
+    //         var oldSetter = Object.getOwnPropertyDescriptor(prt, evt).set;
+    //         Object.defineProperty(prt, evt, {
+    //             set: function(h) {
+    //                 if (h != null) {
+    //                     plt_registerListener(this, evt, h, false, true);
+    //                 } else {
+    //                     // Wipe all listeners for this event type.
+    //                     plt_deregisterListener(this, evt, h, false, true);
+    //                 }
+    //                 oldSetter.call(this, h);
+    //             }});
+    //     });
+    // });
 })();
 
 var __tracer = new (function(){
