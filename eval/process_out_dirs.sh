a8ea1f6ce3765a6b52775f26c4794f0bda9fc9ff
goelayu@umich.edu <goelayu@umich.edu>
2021-11-10 10:53:08 -0500

    update eval based on lastest pipeline

--- eval/process_out_dirs.sh
+++ eval/process_out_dirs.sh
@@ -3,36 +3,55 @@
 # This script processes the final output
 # directory and generates storage and performance
 # results
-set -v
+# set -v
 
 script_dir='/vault-home/goelayu/webArchive/scripts'
 src_dir='/y/goelayu/webArchive/data/out_dirs/'
 raw_dir='/x/goelayu/webArchive/data/raw_dirs/'
-result_dir='/vault-home/goelayu/webArchive/results/SOSP21/eval'
-while read site; do 
-    echo Executing site $site: ${src_dir}/${site}.tar 
-    # cp ${_src_dir}/${site}.tar ${src_dir}
-    # echo Done moving tar, extracing
-    start=`date +%s`
-    # tar xf ${src_dir}/${site}.tar -C ${raw_dir}
-    end=`date +%s`
-    echo Done extracting tar in time: $((end-start))
-    cd $script_dir
-    node all-mime-storage.js -d $raw_dir/$site/instOutput -u <(cat /vault-home/goelayu/webArchive//data/500k/stats/fns/$site | cut -d/ -f2,3 ) \
-    -o $result_dir/storage/pageMD/${site}.other &> $result_dir/storage/all_resources/$site
-    node --max-old-space-size=20000 js-formats/fn-cmp.js -d $raw_dir/$site/instOutput/ -u <(cat /vault-home/goelayu/webArchive//data/500k/stats/fns/$site| cut -d/ -f2,3 | sort -t/ -nk2 ) \
-    -v -p $raw_dir/$site/performance/ -o $result_dir/storage/pageMD/$site &> $result_dir/storage/js/resources/$site && \
-    node index-lookups.js --jd $result_dir/storage/pageMD/$site --alld $result_dir/storage/pageMD/${site}.other --src $raw_dir/$site/instOutput &> $result_dir/storage/js/index/$site &
-    node --max-old-space-size=20000 js-formats/fn-cmp.js -d $raw_dir/$site/instOutput/ -u <(cat /vault-home/goelayu/webArchive//data/500k/stats/fns/$site| cut -d/ -f2,3 | sort -t/ -nk2 ) \
-    -v -p $raw_dir/$site/performance/ -o $result_dir/storage/pageMD/${site}.filter --filter &> $result_dir/storage/js/resources/${site}.filter && \
-    node index-lookups.js --jd $result_dir/storage/pageMD/${site}.filter --alld $result_dir/storage/pageMD/${site}.other --src $raw_dir/$site/instOutput &> $result_dir/storage/js/index/${site}.filter &
-    echo Done launching scripts, Running in bg $site
-    echo Waiting for bg jobs
-    wait $(jobs -p)
-    # echo Cleaning up sites
-    # rm -r ${src_dir}/${site}.tar ${src_dir}/${site}
-done<$1
+result_dir='/vault-home/goelayu/webArchive/results/OSDI22/eval'
+# while read site; do 
+#     echo Executing site $site: ${src_dir}/${site}.tar 
+#     # cp ${_src_dir}/${site}.tar ${src_dir}
+#     # echo Done moving tar, extracing
+#     start=`date +%s`
+#     # tar xf ${src_dir}/${site}.tar -C ${raw_dir}
+#     end=`date +%s`
+#     echo Done extracting tar in time: $((end-start))
+#     cd $script_dir
+#     # node all-mime-storage.js -d $raw_dir/$site/instOutput -u <(cat /vault-home/goelayu/webArchive//data/500k/stats/fns/$site | cut -d/ -f2,3 ) \
+#     # -o $result_dir/storage/pageMD/${site}.other &> $result_dir/storage/all_resources/$site
+#     node --max-old-space-size=20000 js-formats/fn-cmp.js -d $raw_dir/$site/instOutput/ -u <(cat /vault-home/goelayu/webArchive//data/500k/stats/fns/$site| cut -d/ -f2,3 | sort -t/ -nk2 ) \
+#     -v -p $raw_dir/$site/performance/ -o $result_dir/storage/pageMD/$site &> $result_dir/storage/js/resources/$site && \
+#     node index-lookups.js --jd $result_dir/storage/pageMD/$site --alld $result_dir/storage/pageMD/${site}.other --src $raw_dir/$site/instOutput &> $result_dir/storage/js/index/$site &
+#     node --max-old-space-size=20000 js-formats/fn-cmp.js -d $raw_dir/$site/instOutput/ -u <(cat /vault-home/goelayu/webArchive//data/500k/stats/fns/$site| cut -d/ -f2,3 | sort -t/ -nk2 ) \
+#     -v -p $raw_dir/$site/performance/ -o $result_dir/storage/pageMD/${site}.filter --filter &> $result_dir/storage/js/resources/${site}.filter && \
+#     node index-lookups.js --jd $result_dir/storage/pageMD/${site}.filter --alld $result_dir/storage/pageMD/${site}.other --src $raw_dir/$site/instOutput &> $result_dir/storage/js/index/${site}.filter &
+#     echo Done launching scripts, Running in bg $site
+#     echo Waiting for bg jobs
+#     wait $(jobs -p)
+#     # echo Cleaning up sites
+#     # rm -r ${src_dir}/${site}.tar ${src_dir}/${site}
+# done<$1
 
 # echo 'Waiting for background jobs to finish'
 # wait $(jobs -p)
 # echo 'Done'
+
+src_dir='/x/goelayu/webArchive/data/raw_dirs/storage-mix-large/500k-snapshot/'
+proc_dir='/w/goelayu/webArchive/data/'
+result_dir='/vault-home/goelayu/webArchive/results/OSDI22/misc/500k-snapshot/storage'
+while read crawler; do 
+    echo Executing $crawler
+    cd $proc_dir
+    tar -xf ${src_dir}/${crawler}/${crawler}.performance.tar
+    tar -xf ${src_dir}/${crawler}/${crawler}.instOutput.tar
+    cd $script_dir
+
+    node --max-old-space-size=20000 js-formats/fn-cmp.js -d $proc_dir/$crawler/instOutput/ -u <( cd $proc_dir && find . -iname allFns | cut -d/ -f4,5,6) \
+    -v -p $proc_dir/$crawler/performance/ &> $result_dir//$crawler
+
+    #cleanup
+    cd $proc_dir
+    rm -r $crawler
+
+done<$1
--- eval/run_inst_load_add.sh
+++ eval/run_inst_load_add.sh
@@ -3,45 +3,57 @@
 
 src_dir='/w/goelayu/webArchive/data'
 
-inst_cmd_dir='/vault-home/goelayu/webArchive/data/500k/stats/inst_cmds'
-replay_cmd_dir='/vault-home/goelayu/webArchive/data/500k/stats/replay_cmds'
+inst_cmd_dir='/vault-home/goelayu/webArchive/data/storage-mix-large/500k-snapshot/stats/inst_cmds'
+replay_cmd_dir='/vault-home/goelayu/webArchive/data/storage-mix-large/500k-snapshot/stats/replay_cmds'
 
-replay_log_dir='/vault-home/goelayu/webArchive/logs/500k/'
+replay_log_dir='/vault-home/goelayu/webArchive/logs/storage-mix-large/500k-snapshot/'
 
 fin_tars_dir='/vault-home/goelayu/webArchive/data/500k/fin_tars'
 src_tars_dir='/vault-home/goelayu/webArchive/data/500k/src_tars'
-raw_dir='/x/goelayu/webArchive/data/raw_dirs/'
+raw_dir='/x/goelayu/webArchive/data/raw_dirs/storage-mix-large/500k-snapshot'
 
 log_dir='/vault-home/goelayu/webArchive/logs/NSDI22/add_crawls/inst'
 
 #  change directory to SSD
-mkdir -p /w/goelayu/webArchive/data/sites/
-cd /w/goelayu/webArchive/data/sites/
+mkdir -p /w/goelayu/webArchive/data/
+cd /w/goelayu/webArchive/data/
 
-while read site; do 
-    cd /w/goelayu/webArchive/data/sites/
-    mkdir -p $site/orig/;
-    cd $site;
-    echo 'Copying tar for site', $site
+while read crawler; do 
+    cd /w/goelayu/webArchive/data/
+    mkdir -p $crawler/orig/;
+    mkdir -p ${raw_dir}/$crawler/
+    cd $crawler;
+    echo 'Copying tar for site', $crawler
     # #extract the tar
-    cp /vault-home/goelayu/webArchive/data/500k/add_crawls/set1/${site}/${site}.tar.gz .
-    echo 'done copying tar'
+    # cp /vault-home/goelayu/webArchive/data/500k/add_crawls/set1/${site}/${site}.tar.gz .
+    start=`date +%s`
+    tar -xzf /vault-home/goelayu/webArchive/data/storage-mix-large/500k-snapshot/$crawler/record.tar.gz
+    end=`date +%s`
+    runtime=$((end-start))
+    # echo 'done copying tar'
     # #extract the tar
-    tar xf ${site}.tar.gz
-    echo 'done extracting tar'
+    # tar xf ${site}.tar.gz
+    echo 'done extracting tar' $runtime
     # mv data to correct directory
     # # [ -d data ] && mv data/*/*/record/$site/* orig/  && rm -r data
-    [ -d $site ] && mv $site/* orig/ && rm -r $site
-    rm -r ${site}.tar.gz
+    [ -d record ] && mv record/* orig/
+    # rm -r ${site}.tar.gz
     echo 'done moving data'
     # run inst commands in program_analysis directory
     cd /vault-home/goelayu/webArchive/program_analysis
-    cat ${inst_cmd_dir}/add/${site}| parallel --max-proc 5 &> ${log_dir}/$site
-    # echo 'done launching inst commands from', $(pwd)
+    # cat ${inst_cmd_dir}//${crawler} | grep 20201101051005 
+    start=`date +%s`
+    cat ${inst_cmd_dir}//${crawler} | parallel --max-proc 5 &> ${replay_log_dir}/${crawler}.inst
+    end=`date +%s`
+    runtime=$((end-start))
+    echo 'done launching inst commands from', $(pwd) $runtime
     # # load pages in chrome
-    # cd /vault-home/goelayu/webArchive/scripts/
-    # cat ${replay_cmd_dir}/${site} | parallel --max-proc 10 &> ${replay_log_dir}/${site}
-    echo 'done launching replay commands from', $(pwd)
+    cd /vault-home/goelayu/webArchive/scripts/
+    start=`date +%s`
+    cat ${replay_cmd_dir}/${crawler} | parallel --max-proc 30 &> ${replay_log_dir}/${crawler}.replay
+    end=`date +%s`
+    runtime=$((end-start))
+    echo 'done launching replay commands from', $(pwd) $runtime
 
     #compute total allFns while and then tar the files
     # cd /w/goelayu/webArchive/data/sites/$site
@@ -49,12 +61,18 @@ while read site; do
 
     # cd ../ && tar zcf $site.tar.gz $site && mv $site.tar.gz $fin_tars_dir && rm -r $site &
     echo Done running all scripts. Creating tars separately - src and out
-    cd /w/goelayu/webArchive/data/sites/
-    tar zcf $site.mod.tar.gz $site/mod && mv $site.mod.tar.gz $raw_dir/mods/ && rm -r $site/mod
-    rsync -a $site/instOutput/* $raw_dir/$site/instOutput && rm -r $site/instOutput
-    rm -r $site
+    cd /w/goelayu/webArchive/data/
+    # tar zcf $site.mod.tar.gz $site/mod && mv $site.mod.tar.gz $raw_dir/mods/ && rm -r $site/mod
+    # rsync -a $site/instOutput/* $raw_dir/$site/instOutput && rm -r $site/instOutput
+    # create tars for both instOutput and performance since they are needed for analysis, delete everything else
+    start=`date +%s`
+    tar -cf ${crawler}.performance.tar $crawler/performance && mv ${crawler}.performance.tar ${raw_dir}/$crawler/
+    tar -cf ${crawler}.instOutput.tar $crawler/instOutput && mv ${crawler}.instOutput.tar ${raw_dir}/$crawler/
+    rm -r $crawler
+    end=`date +%s`
+    runtime=$((end-start))
     # tar --use-compress-program=pigz -cf $site.tar.src.pigz $site/orig $site/mod && mv $site.tar.src.pigz $src_tars_dir
     # tar --use-compress-program=pigz -cf ../out_tars/$site.tar.out.pigz $site/instOutput $site/performance && rm -r $site
 
-    echo 'Site ' $site, ' finished...'
+    echo 'Site ' $crawler, ' finished...'
 done<$1
