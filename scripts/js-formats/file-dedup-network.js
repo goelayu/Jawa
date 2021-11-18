fee97a63f2d440b5fc972e68b3d2f84fd378ace3
goelayu@umich.edu <goelayu@umich.edu>
2021-11-18 11:00:12 -0500

    add filter analysis to dedup network analysis

--- scripts/js-formats/file-dedup-network.js
+++ scripts/js-formats/file-dedup-network.js
@@ -5,7 +5,12 @@
 
 var fs = require('fs'),
     program = require('commander'),
-    netParser = require('parser/networkParser');
+    netParser = require('parser/networkParser'),
+    blocker = require('@cliqz/adblocker');
+
+const Request = blocker.Request;
+const EASYPRIVACY=`${__dirname}/../../filter-lists/combined.txt`;
+const CUSTOMLIST=`${__dirname}/../../filter-lists/alexa-3k`;
 
 program
     .option('-n, --network [network]', 'file containing network file paths')
@@ -47,7 +52,33 @@ var timeStampInURL = function (url) {
     return false
 }
 
-var updateDataStore = function(n, dataStore){
+var initBlockerEngines = function(){
+    var engineFactory = blocker.FiltersEngine;
+    var engineADBL = engineFactory.parse(fs.readFileSync(EASYPRIVACY, 'utf-8')),
+        engineCUSTOM = engineFactory.parse(fs.readFileSync(CUSTOMLIST, 'utf-8'));
+    
+    return [engineADBL, engineCUSTOM];
+}
+
+var _filterCheck = function(engine, url, sourceURL){
+    return engine.match(Request.fromRawDetails({
+        type:'script',
+        url:url,
+        sourceUrl: sourceURL
+    })).match;
+
+}
+
+var checkForFilteredURLs = function(url, sourceURL, engines){
+    var filter_adblk = filter_custom = false;
+
+    filter_adblk = _filterCheck(engines[0], url, sourceURL);
+    filter_custom = _filterCheck(engines[1], url, sourceURL);
+
+    return [filter_adblk, filter_custom];
+}
+
+var updateDataStore = function(n, dataStore, engines){
     // n is the network file
     var net = netParser.parseNetworkLogs(JSON.parse(fs.readFileSync(n, 'utf-8')));
     for (var n of net){
@@ -59,6 +90,15 @@ var updateDataStore = function(n, dataStore){
         if (dataStore[type].keys.indexOf(key)<0){
             dataStore[type].dedup += n.size
             dataStore[type].keys.push(key);
+
+            //check against filter lists
+            if (type == 'js'){
+                var [filter_adblk, filter_custom] = checkForFilteredURLs(n.url, net.documentURL, engines);
+                !filter_adblk && (dataStore[type].filter_adblk += n.size);
+                !filter_custom && (dataStore[type].filter_custom += n.size);
+                (!filter_custom && !filter_adblk) && (dataStore[type].filter_both += n.size);
+            }
+
         }
         dataStore[type].total += n.size;
     }
@@ -69,19 +109,21 @@ var main = function(){
 
     var dataStore = {
         other: {total:0, dedup:0, keys:[]},
-        js: {total:0, dedup:0, keys:[]}
+        js: {total:0, dedup:0, filter_adblk:0, filter_custom:0, filter_both: 0, keys:[]}
     }
+
+    var [engineADBL, engineCUSTOM] = initBlockerEngines();
 
     pages.split('\n').forEach((p)=>{
         if (p == '') return;
         try {
-            updateDataStore(p, dataStore);
+            updateDataStore(p, dataStore, [engineADBL, engineCUSTOM]);
         } catch (e){
             program.verbose && console.log(`error while parsing network file: ${e}`);
         }
 
     })
-    console.log(dataStore.js.total, dataStore.js.dedup, dataStore.other.total, dataStore.other.dedup);
+    console.log(dataStore.other.total, dataStore.other.dedup,dataStore.js.total, dataStore.js.dedup, dataStore.js.filter_adblk, dataStore.js.filter_custom, dataStore.js.filter_both);
 }
 
 main();
