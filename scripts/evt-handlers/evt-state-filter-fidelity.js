c102748ef1c145b998f7b426085451a480a01b30
goelayu@umich.edu <goelayu@umich.edu>
2021-11-22 14:08:40 -0500

    non-trivial event state comparison

--- scripts/evt-handlers/evt-state-filter-fidelity.js
+++ scripts/evt-handlers/evt-state-filter-fidelity.js
@@ -11,6 +11,7 @@ const fs = require('fs'),
 program 
     .option('-a, --all [all]', 'path to evt state for all js files')
     .option('-f, --filtered [filtered]','path to evt state post filtering')
+    .option('-v, --verbose', 'enable verbose logging')
     .parse(process.argv);
 
 
@@ -18,22 +19,81 @@ var parse = function(f){
     return JSON.parse(fs.readFileSync(f, 'utf-8'));
 }
 
-var compareState = function(allstate, filteredstate){
+var filterAndStrState = function(state){
+    var res = state;
+    //filter
+    Object.keys(res).forEach((evt)=>{
+        res[evt] = res[evt].filter(e=>e[2].indexOf('__')<0);
+    })
+    //str state
+    Object.keys(res).forEach((evt)=>{
+        res[evt].forEach((entry,ind)=>{
+            res[evt][ind] = JSON.stringify(entry);
+        });
+    });
+    return res;
+}
+
+var compareStateSimple = function(allstate, filteredstate){
     return JSON.stringify(allstate) == JSON.stringify(filteredstate);
 }
 
+var compareState = function(all, filteredstate, handlers){
+    // if (!allstate || !allstate.length) return true;
+    var res = false, mismatches = 0;
+    loop1: for (var entry of filteredstate){
+        var found = false;
+        loop2: for (var h of handlers){
+            var allstate = all[h];
+            if (!allstate || !allstate.length){
+                found = true;
+                break;
+            }
+            if (allstate.indexOf(entry)>=0){
+                found = true;
+                break;
+            }
+        }
+        if (!found){
+            mismatches++;
+            program.verbose && console.log(`no entry found for ${entry}`)
+            // return false;
+        }
+        // if (allstate.indexOf(entry)<0){
+        //     console.log(`no entry found for ${entry}`)
+        //     return false;
+        // }
+    }
+    return mismatches;
+}
+
+var getMainElems = function(evtHandlers){
+    var elemsToInvocs = {};
+    for (var handler of evtHandlers){
+        var elem = handler.split('_on')[0];
+        if (!elemsToInvocs[elem])
+            elemsToInvocs[elem] = [];
+        elemsToInvocs[elem].push(handler);
+    }
+    return elemsToInvocs;
+}
+
 var stateMatches = function(all, filtered){
-    all = parse(all),
-        filtered = parse(filtered);
+    all = filterAndStrState(parse(all)),
+        filtered = filterAndStrState(parse(filtered));
+
 
     var evtHandlers = Object.keys(filtered);
-
+    var elemsToInvocs = getMainElems(evtHandlers);
+    var mismatch = 0;
     for (var handler of evtHandlers){
-        var isMatch = compareState(all[handler], filtered[handler]);
-        if (!isMatch)
-            return false;
+        program.verbose && console.log(handler)
+        var elem = handler.split('_on')[0];
+        mismatch += compareState(all, filtered[handler], elemsToInvocs[elem]);
+        // if (!isMatch)
+        //     return false;
     }
-    return true;
+    return mismatch;
 }
 
 console.log(stateMatches(program.all, program.filtered));
