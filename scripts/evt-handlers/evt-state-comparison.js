f07106dbdecea1672af0c458bc961018109a2134
goelayu@umich.edu <goelayu@umich.edu>
2021-12-03 19:05:54 -0500

    update event handler analysis

--- scripts/evt-handlers/evt-state-comparison.js
+++ scripts/evt-handlers/evt-state-comparison.js
@@ -16,82 +16,84 @@ var fs = require('fs'),
 program
     .option('-s, --state [state]', 'path to the state access')
     .option('-h, --handlers [handlers]', ' path to the list of handlers')
-    .option('-i, --ids [ids]','list of files containing the static analysis information')
+    .option('-i, --ids [ids]', 'list of files containing the static analysis information')
+    .option('-v, --verbose', 'verbose output')
     .parse(process.argv);
 
 var fnToBranches = {};
-var getBranchedFunctions = function(){
+var getBranchedFunctions = function () {
     var filterTokens = [
-        'test(', 'concat(', 'clearTimeout(', 'appendChild', 'getComputedChild','push(', 'hasClass(',
-        'attr(', '.replace(', 'Math.','.filter(', 'banner','dataLayer', 'removeClass', 'doneResult',
-        '.each(','.dispatchExecution'
+        'test(', 'concat(', 'clearTimeout(', 'appendChild', 'getComputedChild', 'push(', 'hasClass(',
+        'attr(', '.replace(', 'Math.', '.filter(', 'banner', 'dataLayer', 'removeClass', 'doneResult',
+        '.each(', '.dispatchExecution'
     ]
-    var files = fs.readFileSync(program.ids, 'utf-8').split('\n').filter(e=>e!="");
-    files.forEach((f)=>{
+    var files = fs.readFileSync(program.ids, 'utf-8').split('\n').filter(e => e != "");
+    files.forEach((f) => {
         var content;
         try {
             var content = JSON.parse(fs.readFileSync(f, 'utf-8'));
-        } catch (e){
+        } catch (e) {
             content = {};
         }
-        Object.keys(content).forEach((fn)=>{
-            fnToBranches[fn] = content[fn].filter(e=>!filterTokens.find(t=>e[1].indexOf(t)>=0));
+        Object.keys(content).forEach((fn) => {
+            fnToBranches[fn] = content[fn].filter(e => !filterTokens.find(t => e[1].indexOf(t) >= 0));
         })
     })
     return fnToBranches;
 }
 
-var isBranchedFn = function(fnToBranches, fn){
+var isBranchedFn = function (fnToBranches, fn) {
+    return true;
     // console.log(fnToBranches[fn])
-    if (fn in fnToBranches && fnToBranches[fn].length > 0){
-        console.log(fnToBranches[fn])
+    if (fn in fnToBranches && fnToBranches[fn].length > 0) {
+        program.verbose && console.log(fnToBranches[fn])
         return true;
     }
 }
-var parseHandlers = function(){
+var parseHandlers = function () {
     var handlers = JSON.parse(fs.readFileSync(program.handlers));
     var eventToHandler = {};
-    handlers.forEach((h)=>{
+    handlers.forEach((h) => {
         var [elem, evtDict] = h;
-        Object.keys(evtDict).forEach((evt)=>{
+        Object.keys(evtDict).forEach((evt) => {
             var fn = evtDict[evt][0];
             var key = `${elem}_on${evt}`;
-            eventToHandler[key]=fn;
+            eventToHandler[key] = fn;
         })
     });
     return eventToHandler;
 }
-var pruneReadsAfterWrites = function(state){
+var pruneReadsAfterWrites = function (state) {
     var scopesMem = {}, final = [];
-    state.forEach((st)=>{
-        if (st[0].indexOf('_reads') > 0 ) return;
+    state.forEach((st) => {
+        if (st[0].indexOf('_reads') > 0) return;
         var scope = st[0].split('_writes')[0];
         if (!(scope in scopesMem))
             scopesMem[scope] = [];
         scopesMem[scope].push(st[1]);
         final.push(st);
     })
-    state.forEach((st)=>{
-        if (st[0].indexOf('_reads') < 0 ) return;
+    state.forEach((st) => {
+        if (st[0].indexOf('_reads') < 0) return;
 
         var scope = st[0].split('_reads')[0];
-        if (scope in scopesMem && 
-            scopesMem[scope].indexOf(st[1])>=0)
+        if (scope in scopesMem &&
+            scopesMem[scope].indexOf(st[1]) >= 0)
             return;
         final.push(st);
     })
     return final;
 }
 
-var _parseState = function(state){
-    var pState = {DOMS:[], global:[], closure:[]},
+var _parseState = function (state) {
+    var pState = { DOMS: [], global: [], closure: [] },
         pRW = pruneReadsAfterWrites;
-    state.forEach((st)=>{
-        if (st[0].indexOf('jquery')>=0 || (st[1].indexOf && st[1].indexOf('guid')>=0)) return;
-        if (typeof st[1] != "string" || st[1].indexOf('undefined')>=0) return;
-        if (st[0].indexOf('DOMS')==0) pState.DOMS = st[1].filter(e=>e[1].indexOf('dispatch')<0);
-        if (st[0].indexOf('global_')>=0) pState.global.push(st)
-        if (st[0].indexOf('closure_')>=0) pState.closure.push(st);
+    state.forEach((st) => {
+        if (st[0].indexOf('jquery') >= 0 || (st[1].indexOf && st[1].indexOf('guid') >= 0)) return;
+        if (typeof st[1] != "string" || st[1].indexOf('undefined') >= 0) return;
+        if (st[0].indexOf('DOMS') == 0) pState.DOMS = st[1].filter(e => e[1].indexOf('dispatch') < 0);
+        if (st[0].indexOf('global_') >= 0) pState.global.push(st)
+        if (st[0].indexOf('closure_') >= 0) pState.closure.push(st);
 
     });
     // pState.global = pRW(pState.global);
@@ -99,58 +101,58 @@ var _parseState = function(state){
     return pState;
 }
 
-var parseState = function(state){
+var parseState = function (state) {
     var fState = {};
-    Object.keys(state).forEach((elem)=>{
+    Object.keys(state).forEach((elem) => {
         fState[elem] = _parseState(state[elem]);
     });
     return fState;
 }
 
-var isRWConflict = function(s1, s2){
-    if (s1[0].indexOf('_read')>=0 && s2[0].indexOf('_write')>=0) return s1;
-    if (s1[0].indexOf('_read')>=0 && s2[0].indexOf('_write')>=0) return s2;
+var isRWConflict = function (s1, s2) {
+    if (s1[0].indexOf('_read') >= 0 && s2[0].indexOf('_write') >= 0) return s1;
+    if (s1[0].indexOf('_read') >= 0 && s2[0].indexOf('_write') >= 0) return s2;
 
     return false;
 }
 
-var isRWConflictDOM = function(type1, type2){
-    if ((type1.indexOf('get')>=0 && type2.indexOf('set')>=0) ||
-        (type2.indexOf('get')>=0 && type1.indexOf('set')>=0))
+var isRWConflictDOM = function (type1, type2) {
+    if ((type1.indexOf('get') >= 0 && type2.indexOf('set') >= 0) ||
+        (type2.indexOf('get') >= 0 && type1.indexOf('set') >= 0))
         return true;
 
     return false;
 }
 
 
-var getClosureScope = function(closureLog){
+var getClosureScope = function (closureLog) {
     var read = "_read", write = "_write";
-    if (closureLog.indexOf(read)>=0)
+    if (closureLog.indexOf(read) >= 0)
         return closureLog.split(read)[0]
     else return closureLog.split(write)[0];
 }
 
-var overlapHeap = function(h1, h2){
+var overlapHeap = function (h1, h2) {
     //compare global    
-    for (var g1 of h1.global){
-        for (var g2 of h2.global){
+    for (var g1 of h1.global) {
+        for (var g2 of h2.global) {
             var conflict;
-            if (g1[1] == g2[1] && (conflict = isRWConflict(g1, g2))){
-                if (!isBranchedFn(fnToBranches,conflict[3])) continue;
-                console.log(g1, g2)
+            if (g1[1] == g2[1] && (conflict = isRWConflict(g1, g2))) {
+                if (!isBranchedFn(fnToBranches, conflict[3])) continue;
+                program.verbose && console.log(g1, g2)
                 return true;
             }
         }
     }
 
-    for (var c1 of h1.closure){
+    for (var c1 of h1.closure) {
         var cs1 = getClosureScope(c1[0]);
-        for (var c2 of h2.closure){
+        for (var c2 of h2.closure) {
             var cs2 = getClosureScope(c2[0]);
             var conflict;
-            if (c1[1] == c2[1] && cs1 == cs2 && (conflict = isRWConflict(c1, c2)) ){
-                if (!isBranchedFn(fnToBranches,conflict[3])) continue;
-                console.log(c1, c2)
+            if (c1[1] == c2[1] && cs1 == cs2 && (conflict = isRWConflict(c1, c2))) {
+                if (!isBranchedFn(fnToBranches, conflict[3])) continue;
+                program.verbose && console.log(c1, c2)
                 return true;
             }
         }
@@ -158,10 +160,10 @@ var overlapHeap = function(h1, h2){
     return false;
 }
 
-var overlapDOM = function(s1, s2){
-    for (var d1 of s1.DOMS){
-        for (var d2 of s2.DOMS){
-            if (d1[0] == d2[0] && d1[2] == d2[2] && isRWConflictDOM(d1[1], d2[1])){
+var overlapDOM = function (s1, s2) {
+    for (var d1 of s1.DOMS) {
+        for (var d2 of s2.DOMS) {
+            if (d1[0] == d2[0] && d1[2] == d2[2] && isRWConflictDOM(d1[1], d2[1])) {
                 // console.log(d1, d2)
                 return true;
             }
@@ -170,43 +172,43 @@ var overlapDOM = function(s1, s2){
     return false;
 }
 
-var isPairedEvent = function(e1, e2){
-    var pairs = [["mouseup", "mousedown"],["mouseleave","mousemove"], ["mousemove", "mouseup"]]
-    for (p of pairs){
-        if ((e1.indexOf(p[0])>=0 && e2.indexOf(p[1])>=0) ||
-        (e1.indexOf(p[1])>=0 && e2.indexOf(p[0])>=0))
+var isPairedEvent = function (e1, e2) {
+    var pairs = [["mouseup", "mousedown"], ["mouseleave", "mousemove"], ["mousemove", "mouseup"]]
+    for (p of pairs) {
+        if ((e1.indexOf(p[0]) >= 0 && e2.indexOf(p[1]) >= 0) ||
+            (e1.indexOf(p[1]) >= 0 && e2.indexOf(p[0]) >= 0))
             return true;
     }
     return false;
 }
 
-var compareState = function(state){
+var compareState = function (state) {
     var elems = Object.keys(state);
     var eventToHandler = parseHandlers();
-    for (var _e1 = 0; _e1 < elems.length-1; _e1++){
+    for (var _e1 = 0; _e1 < elems.length - 1; _e1++) {
         var e1 = elems[_e1];
-        if (e1.indexOf('#document_')>=0) continue;
-        if (overlapHeap(state[e1], state[e1])){
+        if (e1.indexOf('#document_') >= 0) continue;
+        if (overlapHeap(state[e1], state[e1])) {
             console.log(e1, e1)
             console.log(`Heap overlap`)
             return true;
         }
-        for (var _e2 = _e1+1; _e2 < elems.length;_e2++){
+        for (var _e2 = _e1 + 1; _e2 < elems.length; _e2++) {
             var e2 = elems[_e2];
-            if (e2.indexOf('#document_')>=0) continue;
-            if (isPairedEvent(e1,e2)) continue;
+            if (e2.indexOf('#document_') >= 0) continue;
+            if (isPairedEvent(e1, e2)) continue;
             var e1key = e1.split("_count")[0], e2key = e2.split("_count")[0];
             if (eventToHandler[e1key] == eventToHandler[e2key]) {
                 // console.log('same fn')
                 continue;
             }
-            if (overlapHeap(state[e1], state[e2])){
+            if (overlapHeap(state[e1], state[e2])) {
                 console.log(e1, e2)
                 console.log(`Heap overlap`)
                 return true;
             }
 
-            if (overlapDOM(state[e1], state[e2])){
+            if (overlapDOM(state[e1], state[e2])) {
                 // console.log('DOM overlap')
                 return true;
             }
@@ -215,8 +217,8 @@ var compareState = function(state){
     return false;
 }
 
-var main = function(){
-    getBranchedFunctions();
+var main = function () {
+    // getBranchedFunctions();
     var state = parseState(JSON.parse(fs.readFileSync(program.state, 'utf-8')));
     console.log(compareState(state));
 }
--- scripts/evt-handlers/input-static.js
+++ scripts/evt-handlers/input-static.js
@@ -9,74 +9,83 @@ var fs = require('fs'),
     zlib = require('zlib');
 
 program
-    .option('-s, --state [state]','path to the js state file')
-    .option('-i, --info [info]','path to instOutput dir containing file infos')
+    .option('-s, --state [state]', 'path to the js state file')
+    .option('-i, --info [info]', 'path to instOutput dir containing file infos')
     .parse(process.argv);
 
-var getCallStack = function(){
+var getCallStack = function () {
     var state = JSON.parse(fs.readFileSync(program.state, 'utf-8'));
     var callStack = {};
-    Object.keys(state).forEach((elem)=>{
-        var _allst = state[elem].map(e=>e[3]).filter(e=>e && typeof e == 'string'),
-            allSt = [...new Set(_allst)];
-        callStack[elem] = allSt;
+    Object.keys(state).forEach((elem) => {
+        // var _allst = state[elem].map(e=>e[3]).filter(e=>e && typeof e == 'string'),
+        //     allSt = [...new Set(_allst)];
+        // callStack[elem] = allSt;
+        var allst = state[elem].filter(e => e.indexOf('hash') >= 0);
+        callStack[elem] = allst;
     })
     return callStack;
 }
 
-var getAllFiles = function(callStack){
+var getAllFiles = function (callStack) {
     var allFiles = new Set;
-    Object.keys(callStack).forEach((elem)=>{
-        var cs = callStack[elem].map(e=>e.split('-function-')[0]);
+    Object.keys(callStack).forEach((elem) => {
+        var cs = callStack[elem].map((f) => {
+            if (f.indexOf('-hash-') < 0) return;
+            var endIndx = 4;
+            if (f.indexOf('-script-') >= 0)
+                endIndx = 6;
+            return f.split('-').slice(0, f.split('-').length - endIndx).join('-');
+        });
         cs.forEach(allFiles.add, allFiles)
     });
     return [...allFiles];
 }
 
-var parseJS = function(fileStore, src,file){
+var parseJS = function (fileStore, src, file) {
     // console.log(`parsing file of length ${src.length}`)
-    var makeId = function(node){
+    var makeId = function (node) {
         var loc = node.loc;
         if (node.id) loc = node.id.loc;
-        return file + '-function-' + loc.start.line + '-'
+        return file + '-' + loc.start.line + '-'
             + loc.start.column + '-'
             + loc.end.line + '-'
             + loc.end.column;
     }
-    falafel({source:src, locations:true}, function(node){
-        if (node.type == 'FunctionDeclaration' || node.type == 'FunctionExpression'){
+    falafel({ source: src, locations: true }, function (node) {
+        if (node.type == 'FunctionDeclaration' || node.type == 'FunctionExpression') {
             var id = makeId(node);
             fileStore[id] = node.source();
         }
     });
 }
 
-var initializeFileData = function(files){
+var initializeFileData = function (files) {
     var fileStore = {};
-    files.forEach((file)=>{
-        try{
+    files.forEach((file) => {
+        try {
             var fileDir = `${program.info}/${file}`;
             var fileInfo = JSON.parse(fs.readFileSync(`${fileDir}/${file}`));
             // console.log(fileDir, file)
-            var content = fs.readFileSync(`${fileDir}/content_full`,'utf-8');
+            var content = fs.readFileSync(`${fileDir}/content_full`, 'utf-8');
             // console.log(fileInfo.zip)
             // if (fileInfo.zip)
             //     content = zlib.gunzipSync(content).toString();
             parseJS(fileStore, content, file);
-        } catch (e){
+        } catch (e) {
             // console.log(e)
         }
     })
     return fileStore;
 }
 
-var grepCallStack = function(cs,fileStore){
+var grepCallStack = function (cs, fileStore) {
     var tokens = ["offsetX", "offsetY", "pageX", "pageY", "screenX", "screenY"],
         found = false;
-    for (var fn of cs){
+    // console.log(cs);
+    for (var fn of cs) {
         var fnSrc = fileStore[fn];
         // console.log(fnSrc)
-        if (fnSrc && (found = tokens.find(e=>fnSrc.indexOf(e)>=0))){
+        if (fnSrc && (found = tokens.find(e => fnSrc.indexOf(e) >= 0))) {
             console.log(fnSrc);
             break;
         }
@@ -85,18 +94,21 @@ var grepCallStack = function(cs,fileStore){
 
 }
 
-var main = function(){
+var main = function () {
     var callStack = getCallStack();
     var allFiles = getAllFiles(callStack);
     var fileStore = initializeFileData(allFiles);
-    for (var elem of Object.keys(callStack)){
-        if (elem.indexOf('onmouse')<0) continue;
+    for (var elem of Object.keys(callStack)) {
+        // console.log(elem, callStack)
+        // if (elem.indexOf('onmouse') < 0) continue;
         var grepRes;
         // console.log(elem)
-        if (grepRes = grepCallStack(callStack[elem], fileStore))
+        if (grepRes = grepCallStack(callStack[elem], fileStore)) {
             console.log(grepRes)
+            return;
+        }
     }
-      
+    console.log(false)
 }
 
 main();
